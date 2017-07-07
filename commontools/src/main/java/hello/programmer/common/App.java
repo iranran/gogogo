f4f4362b4df8717678b03391f86792d6926e108a
LIWEI\YK-DZ-43716 <liweimilan@163.com>
2017-07-07 15:29:36 +0800

    firstcommit

---
+++ commontools/src/main/java/hello/programmer/common/App.java
@@ -0,0 +1,12 @@
+package hello.programmer.common;
+
+/**
+ * @author liwei
+ * @Description
+ * @date 2017/7/7 14:19
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ commontools/src/main/test/java/hello/programmer/common/TestApp.java
@@ -0,0 +1,18 @@
+package hello.programmer.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author liwei
+ * @Description
+ * @date 2017/7/7 14:22
+ */
+public class TestApp {
+
+
+    @Test
+    public void a(){
+        Assert.assertNotNull(null);
+    }
+}
