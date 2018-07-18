ca1101b402d091c0baa183e58a06d6836a890dbc
liwei <liwei@forgame.com>
2018-07-18 12:00:16 +0800

    test

--- learning/src/main/java/hello/programmer/java8/TestLambda.java
+++ learning/src/main/java/hello/programmer/java8/TestLambda.java
@@ -14,7 +14,9 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * (类描述：)
@@ -27,16 +29,19 @@ public class TestLambda {
     public void arraySort(){
         List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
 
-        Collections.sort(names, (String a, String b) -> {
-            return b.compareTo(a);
-        });
+//        Collections.sort(names, (String a, String b) -> {
+//            return b.compareTo(a);
+//        });
 
-        Collections.sort(names, (String a, String b) -> b.compareTo(a));
 
-        Collections.sort(names, (a, b) -> b.compareTo(a));
+
+
+        Collections.sort(names,Comparator.reverseOrder());
 
 
         System.out.println(names);
+
+        System.out.println(names.stream().map(i->"'"+i+"'").collect(Collectors.joining(",")));
     }
 
 
